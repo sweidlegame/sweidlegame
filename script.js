2368025e16a186be3f89c25f63ed5ffdea47396a
nrou2 <nathanrouse5@gmail.com>
2026-04-14 13:52:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let pyodide;
+let pyClick;
+
+async function main() {
+    pyodide = await loadPyodide();
+
+    // Load your Python file
+    let response = await fetch("game.py");
+    let code = await response.text();
+
+    pyodide.runPython(code);
+
+    // Get reference to Python function
+    pyClick = pyodide.globals.get("increment");
+
+    // Hook up button
+    document.getElementById("btn").onclick = () => {
+        let result = pyClick();  // call Python
+        document.getElementById("btn").innerText = "Clicks: " + result;
+    };
+}
+
+main();
