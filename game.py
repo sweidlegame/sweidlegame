5898693c43ca19351203188b953d82a4557c2bb2
nrou2 <nathanrouse5@gmail.com>
2026-04-14 14:27:53 -0400

    Implement Producer class for currency management
    Added a Producer class to manage production and purchasing.

--- game.py
+++ game.py
@@ -1,3 +1,32 @@
+class Producer:
+    def __init__(self,owned,price,production):
+        # constructor (runs when object is created)
+        self.owned = owned
+        self.price = price
+        self.production = production
+        self.upgrademultiplier = 1
+
+    def purchase(self):
+       
+        global currency
+        if currency > price:
+            currency = currency - price
+            self.owned += 1
+            price = price**1.05
+        return
+
+    def produce:
+        return self.owned*self.production*self.multiplier
+
+    def upgrade:
+        self.upgrademodifier += 0.50
+
+
+
+
+
+
+
 currency = 0
 currencypersecond = 1
 
@@ -12,3 +41,4 @@ def second():
 def CPS():
     global currencypersecond
     return currencypersecond
+
