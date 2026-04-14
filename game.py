aa45804122775f34470f06de0b002dd96c121c8d
nrou2 <nathanrouse5@gmail.com>
2026-04-14 14:05:40 -0400

    Refactor currency increment logic and add second function
    Modified increment function to increase currency based on a rate of currencypersecond divided by 0.5. Added second function to increment currency by currencypersecond.

--- game.py
+++ game.py
@@ -3,5 +3,9 @@ currencypersecond = 1
 
 def increment():
     global currency
-    currency = currency + currencypersecond + 1
+    currency = currency + (currencypersecond/0.5)
+    return currency
+def second():
+    global currency
+    currency = currency + currencypersecond
     return currency
