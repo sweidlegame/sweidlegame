883a7c5ae51d03b8c0f0d90c0d5bf98c58cca78d
nrou2 <nathanrouse5@gmail.com>
2026-04-28 18:55:00 -0400

    Refactor script.js to improve readability and structure

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let pyodide;
-
+let one = 1
 // Placeholders for Python functions. We will 'capture' these from game.py later.
 let pyClick, pySecond, pyCPS;
 let pyBuyProducer, pyGetPrice, pyGetOwned, pyGetProduction;
