33c0a75e7abee431898ca196c1f380364d222241
nrou2 <nathanrouse5@gmail.com>
2026-04-28 20:41:30 -0400

    Refactor game.py for improved structure and clarity

--- game.py
+++ game.py
@@ -76,7 +76,7 @@ class Simon:
         return random.choice(["red", "green", "blue","yellow"])
         
     def step(self):
-        self.sequence = sequence.append(genentry())
+        self.sequence.append(genentry())
     def attempt(self,playerseq):
         i = 0
         while i < len(self.sequence):
